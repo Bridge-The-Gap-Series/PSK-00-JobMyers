dbf99406342c2171fa5829759a3c955431f05329
JobMyers <57264631+JobMyers@users.noreply.github.com>
2019-11-03 19:35:10 +0300

    Update main.py
    Update mai,py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello World")
+import statistics
+name=input("Enter your name: ")
+age=int(input("Enter your age(20+): "))
+print("\n\n")
+print("Hello world\n")
+print("my name is",name)
+print("I am ",age," years of age.Young, right?\U0001F600\n")
+numlist=[]
+numlist.append(12)
+numlist.append(4)
+numlist.append(56)
+numlist.append(17)
+numlist.append(8)
+numlist.append(99)
+x=max(numlist)
+print("The maximum number in this list:",numlist,"is",x)
+y=statistics.mean(numlist)
+print("The mean:",numlist,"is",y,"\n")
+allist=["A for Apple","B for Boy","C for Cow","...","Z for Zebra"]
+print(*allist, sep = "\n")
